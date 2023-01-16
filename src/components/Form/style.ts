7195c3f22b3ecfa679bcdb7bfb21bd02158c140c
fernando-izar <fizar@me.com>
2023-01-16 18:26:00 -0300

    feat: RequestValues style ok

--- src/components/Form/style.ts
+++ src/components/Form/style.ts
@@ -1,4 +1,47 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+
+    form {
+        display: flex;
+        flex-direction: column;
+        gap: 15px;
+        margin-top: 20px;
+
+        
+        div {
+            input {
+                width: 320px;
+                height: 40px;
+                border-radius: 5px;
+                border: 1px solid #e6e8eb;
+            }
+
+            input:hover {
+                border: 1px solid blue;
+            }
+            
+            .max-installments {
+                font-size: 12px;
+                color: #a0a0a0;
+            }
+
+            .errors {
+                color: red;
+                font-style: italic;
+            }
+        }  
+        
+        .form-button {
+            height: 30px;
+            width: 30%;
+            border-radius: 5px;
+            margin-top: 20px;
+        }
+
+        .form-button:hover {
+            background-color: #e6e8eb;
+            border: 1px solid #e6e8eb;
+        }
+    }
 `
--- src/components/ResponseValues/style.ts
+++ src/components/ResponseValues/style.ts
@@ -2,6 +2,8 @@ import styled from "styled-components";
 
 export const Container = styled.div`    
 
+    display: flex;
+    flex-direction: column;
     width: 40%;
     background-color: #f7f9fa;
 `
